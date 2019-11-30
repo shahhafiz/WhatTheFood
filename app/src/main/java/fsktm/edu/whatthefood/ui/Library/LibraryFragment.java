10c2aff2e6f2dc0790aa4800ec7dc6dc4cae8c34
DESKTOP-RD29L48\thewise <faizhasnul97@gmail.com>
2019-11-30 17:42:30 +0800

    Added firestore dependency
    1) Added Firestore dependency on build.gradle
    2) Enable multiDex on build.gradle as app when over 64K methods after adding Firestore dependency
    3) Test fetching data from firebase on Library java

--- app/src/main/java/fsktm/edu/whatthefood/ui/Library/LibraryFragment.java
+++ app/src/main/java/fsktm/edu/whatthefood/ui/Library/LibraryFragment.java
@@ -1,6 +1,7 @@
 package fsktm.edu.whatthefood.ui.Library;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import fsktm.edu.whatthefood.R;
 
@@ -17,6 +24,21 @@ public class LibraryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("nutritionvalue")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TEST", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d("TEST", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
         importViewModel = ViewModelProviders.of(this).get(LibraryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_library, container, false);
         return root;
